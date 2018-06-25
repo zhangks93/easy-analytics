1cfaaf225dc966bb7c34a77686ab47647ca78e46
zhangks93 <zhangks93@outlook.com>
2018-06-25 08:36:11 +0800

    add pretty table

--- data_inflow.py
+++ data_inflow.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np
+import prettytable as pt
 def input_data():
     try:
         data= pd.read_excel('./test.xlsx')
@@ -7,9 +8,31 @@ def input_data():
         data = pd.read_csv('./test.csv')
     time=np.unique(data.ix[:,0].values)
     location=np.unique(data.ix[:,0].values)
+    #Imputation of missing values
+    DropNa=data.dropna()
+    imputmissing=data.fillna(data.mean())
+    #get average and standard derivation
+    ave=pd.DataFrame(columns=data.columns)
+    std=pd.DataFrame(columns=data.columns)
     for i in time:
         for j in location:
-            ave=data[((data.time==i) & (data.location==j))].mean()
-            std=data[((data.time==i) & (data.location==j))].std()
-    return data,time,ave,std
-print(input_data())
+            ave=ave.append(data[((data.time==i) & (data.location==j))].mean(),ignore_index=True)
+            std=std.append(data[((data.time==i) & (data.location==j))].std(),ignore_index=True)
+    del ave['sample']
+    del std['sample']
+
+    return data,ave,std
+def Prettytable_DF(df=pd.DataFrame()):
+    table = pt.PrettyTable([''] + list(df.columns))
+    for row in df.itertuples():
+        table.add_row(row)
+    return str(table)
+def Prettytable_ND(ndarray=np.array([])):
+    table = pt.PrettyTable()
+    for row in ndarray:
+        table.add_row(row)
+    return str(table)
+    for i in range(row_number):
+        table.add_row(dataframe.iloc[i,:].values)
+    print(table)
+print(Prettytable_DF(input_data))
